4e2a1148b9875dd7dc4e4e341cc7c2634bf65483
Jasper Kent <software@jasperkent.com>
2022-05-06 08:53:54 +0100

    Add project files.

---
+++ BlazorBookReviews/wwwroot/scripts/interop.js
@@ -0,0 +1,15 @@
+﻿function setHalloween(on) {
+    let body = document.getElementsByTagName("body")[0];
+
+    if (on)
+        body.classList.add("halloween");
+    else
+        body.classList.remove("halloween");
+}
+
+
+function initialize(caller) {
+    let body = document.getElementsByTagName("body")[0];
+
+    body.addEventListener("mouseleave", () => caller.invokeMethodAsync("MouseLeave"));
+}
